44ace25e3a347dea1e931ebb71bf3460d30deb12
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-14 20:10:07 -0500

    Changes to the code

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,4 +1,15 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 
-user1=User()
+user1 = User('001',"Ham")
+
+#Adding attributes to the object 
+
+
+
+
+print(user1.username)
+
+#But this is not practical!
