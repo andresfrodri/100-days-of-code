d862dc1e647b4e2549aa9d4d56ae27104d062ea9
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-14 20:51:47 -0500

    Add files of the project

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,15 +1,33 @@
 class User:
+
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+        pass
 
 user1 = User('001',"Ham")
 
-#Adding attributes to the object 
-
-
-
-
+user2 = User('002',"Cheese")
 print(user1.username)
+user1 = User('001',"Ham")
+
+user2 = User('002',"Cheese")
+
+user1.follow(user2)
+print(user1.followers)
+print(user1.following)
+print(user2.followers)
+print(user2.following)
+
+
+
+
+
 
 #But this is not practical!
---
+++ Day 17/project/main.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for i in range(12):
+    question_bank.append(question_data[i]['text'])
+
+
