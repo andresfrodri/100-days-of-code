bcb44219751ef97da3a8851a19b60af9b5f3952d
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-16 14:27:32 -0500

    Start of day 18

---
+++ Day 18/day18.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+ham_the_turtle = Turtle()
+ham_the_turtle.shape("turtle")
+ham_the_turtle.color('DarkGreen')
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()  #This has to go at the end of the file
