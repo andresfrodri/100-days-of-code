54be48b12c521430c14d782604af1f0d28c94a7b
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-18 21:59:23 -0500

    Add method to move the snake with arrow keys

--- Day 20 & 21/snake.py
+++ Day 20 & 21/snake.py
@@ -1,9 +1,12 @@
+from tkinter import RIGHT
 import turtle as t
+
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         X1=0
@@ -20,5 +23,21 @@ class Snake:
             new_x=self.segments[seg_num-1].xcor()
             new_y=self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(20)
         
+    def up(self):
+        if self.head.heading() != 270:
+           self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+        
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+    
