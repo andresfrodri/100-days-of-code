e4e14c8186e4167e29dbc9b90ac8d21e1bfdec48
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-20 21:58:08 -0500

    added collisions to the walls

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -45,7 +45,14 @@ while game_on:
         score.write(initial ,move = False, align= 'center', font=('consolas', 12, 'normal'))
         snake.new_segment()
 
-    
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False #This freezes the game
+        game_over = Score()
+        game_over.write('GAME OVER',move = False, align= 'center', font=('consolas', 14, 'normal'))
+
+
+
 
         
 
