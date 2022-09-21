ef52380b574a92b1cb228a3bfd000e2a57ef27a5
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-20 21:02:21 -0500

    collision with the food & the sanke

--- Day 20 & 21/food.py
+++ Day 20 & 21/food.py
@@ -8,7 +8,10 @@ class Food(t.Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid= 0.5)
         self.color('red')
+        self.speed(0)
+        self.refresh()
+    
+    def refresh(self):
         random_x = rd.randint(-280,280)
         random_y = rd.randint(-280,280)
-        self.speed(0)
         self.goto(random_x, random_y)
--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -30,6 +30,9 @@ while game_on:
 
     snake.move()
     
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
     
 
         
