72bdae0e341cd56c6aa528a87ae4e83339fa1684
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-28 20:49:12 -0500

    Exploration of some pandas functions

--- Day 25/main.py
+++ Day 25/main.py
@@ -5,16 +5,32 @@ path = r'Day 25\weather_data.csv'
 
 #The way above was awful to use
 
-import  csv
+#import  csv
 
-with open(path, 'r') as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for i in data:
-        if i[1] != 'temp':
-            temperatures.append(int(i[1]))
-
-
+#with open(path, 'r') as data_file:
+#    data = csv.reader(data_file)
+#    temperatures = []
+#    for i in data:
+#        if i[1] != 'temp':
+#            temperatures.append(int(i[1]))
 
 
-print(temperatures)
+
+
+#print(temperatures)
+
+import pandas as pd
+
+data=pd.read_csv(path)
+print(data)
+print(data['temp'])
+
+temp_list=data['temp'].to_list()
+
+print(temp_list)
+
+print(data[data.temp == data['temp'].max()])
+
+monday = data[data.day == 'Monday']
+
+print(monday.condition)
