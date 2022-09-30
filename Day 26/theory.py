7d97b57deaf665dacb3b4c1106430f7b1ac1edc0
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-29 20:48:50 -0500

    Theory complete

---
+++ Day 26/theory.py
@@ -0,0 +1,21 @@
+numbers = [1,2,3,4]
+
+new_list = [n +1 for n in numbers]
+print(new_list)
+
+name = 'Salad'
+new_list = [letter for letter in name]
+print(new_list)
+
+r = range(1,5)
+
+r2=[number*2 for number in r]
+print(r2)
+
+names = ['John','Paul','George', 'Ringo','The Beatles'] 
+
+short_names = [name for name in names if len(name)<5]
+big_names =  [name.upper() for name in names if len(name)>=5]
+
+print(short_names)
+print(big_names)
