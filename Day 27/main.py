a9fdec979516fe9f8beb6620cdae7ebb8ded186f
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-10-04 12:31:46 -0500

    End of the theory of day 27

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,16 +1,34 @@
-import tkinter
+from tkinter import *
+def button_clicked():
+    my_label.config(text=input.get())
 
-window = tkinter.Tk()
+
+window = Tk()
 window.title('First GUI program')
 window.minsize(500, 400)
+window.config(padx=20, pady=20)
 
 #Creating components inside the window
 #Label
 
-my_label = tkinter.Label(text='My fisrt label', font = ('Arial', 24))
-my_label.pack(expand= True)
+my_label = Label(text='My first label', font = ('Arial', 24))
+
+my_label['text']='Hellooo'
+my_label.config(text='Helooooo')
+my_label.grid(column=0, row=0)
+
+#Buttons
+
+button = Button(text='Click me to change title', command= button_clicked)
+button.grid(column=1, row=1)
 
 
+new_button = Button(text='Hi', command= button_clicked)
+new_button.grid(column=2, row=0)
+#Entry
+
+input = Entry(width=13)
+input.grid(column=3, row=2)
 
 
 
---
+++ Day 27/tests.py
@@ -0,0 +1,27 @@
+from pyexpat import model
+
+
+def add(*args):
+    return sum(args)
+
+print(add(1,2,4,56,3,5,3,424,5,0,3,3,2,2))
+
+def calculate(n,**kwargs):
+    n += kwargs['add']
+    print(f'{n-kwargs["add"]} plus {kwargs["add"]} is {n}')
+    n *= kwargs['multiply']    
+    print(f'{n//kwargs["multiply"]} times {kwargs["multiply"]} is {n}')
+    
+
+calculate(2,add=1, multiply=4)    
+
+
+class Car():
+    def __init__(self, **kw):
+        self.make = kw.get('make')
+        self.model = kw.get('model')
+        self.wheels = kw.get('wheels')
+        self.doors = kw.get('doors')
+        
+my_car = Car(make='Honda', doors=3)
+print(my_car.doors)
