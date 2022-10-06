357270a055bc7732eaf5035ea71964f77c234770
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-10-06 16:20:57 -0500

    Changes to project D.29 and end of day 30

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,11 +1,12 @@
 import tkinter
 from tkinter import messagebox
 import random
-
+import json
 import pyperclip
 
 
 FONT_NAME = ('arial', 12)
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 options_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
@@ -20,10 +21,49 @@ def password_gen():
     password_entry.insert(0,s) 
     pyperclip.copy(s)
     
+# ---------------------------- PASSWORD SEARCHER ------------------------------- #
+def searcher():
+    website=website_entry.get()
+    try:
+        with open('data.json','r') as data_file:
+            #Reading old data
+            data=json.load(data_file) #used to read
+            web_dict=data[website]
+    except KeyError:
+        messagebox.showinfo('Error', message='No data found for that website')
+    except FileNotFoundError:
+        messagebox.showinfo('Error', message='No data file found/No saved passwords')
+    else:
+        messagebox.showinfo(website, message=f'Username: {web_dict["username"]} \n Password: {web_dict["password"]}')
+    
 
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def create_data(website, username, password):
+    new_data = {
+        website:{
+            'username': username,
+            'password': password,
+        }    
+    }
+    try:
+        with open('data.json','r') as data_file:
+            #Reading old data
+            data=json.load(data_file) #used to read
+    except FileNotFoundError:
+        with open('data.json','w') as data_file:
+        #creates the files
+            json.dump(new_data, data_file, indent=5) #used to write
+        create_data(website, username, password)
+    else:
+        #Updating old data with new data
+        data.update(new_data)
+        with open('data.json','w') as data_file:
+            #Saving the updating data
+            json.dump(data, data_file, indent=5) #used to write
+
+
 def deleting():
     website_entry.delete(0, 'end')
     username_entry.delete(0, 'end')
@@ -34,14 +74,11 @@ def save():
     website=website_entry.get()
     username=username_entry.get()
     password = password_entry.get()
-    if website == '' or username == '' or password == '':
+    if username == '' or password == '':
         is_ok2=messagebox.showwarning(title='WARNING', message='One of the fields is empty')
     else:
-        is_ok=messagebox.askokcancel(title=website, message=f'These are the details entered: \n Username:{username} \n Is it ok to save?')
-        if is_ok:
-            with open('data.txt','a') as letters:
-                letters.write(website + ' | ' + username + ' | '+ password+'\n')
-            deleting()
+        create_data(website, username, password)
+        deleting()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -73,10 +110,12 @@ generate_button.grid(column=2, row=3)
 add_button = tkinter.Button(text='Add', highlightthickness=0, width=40, command=save)
 add_button.grid(column=1, row=4, columnspan=2,sticky='EW')
 
+search_button = tkinter.Button(text='Search', highlightthickness=0, command=searcher)
+search_button.grid(column=2, row=1,sticky='EW')
 #Entries
 website_entry = tkinter.Entry()
 website_entry.focus()
-website_entry.grid(row=1, column=1,columnspan=2,sticky='EW')
+website_entry.grid(row=1, column=1,sticky='EW')
 
 username_entry = tkinter.Entry()
 username_entry.insert(0, '@gmail.com')
